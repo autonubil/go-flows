6e0ad39f66ad2108c9c0d50764382ddfe713fd4a
Carsten Zeumer <carsten.zeumer@autonubil.de>
2024-06-11 08:18:11 +0200

    implement streaming ipfix exporter

--- modules/exporters/ipfix/ipfix_stream.go
+++ modules/exporters/ipfix/ipfix_stream.go
@@ -21,6 +21,7 @@ type ipfixStreamExporter struct {
 	protocol         string
 	domain           uint32
 	w                net.Conn
+	interval         time.Duration
 	templateLastSent time.Time
 
 	writer    *ipfix.MessageStream
@@ -48,7 +49,7 @@ func (pe *ipfixStreamExporter) Export(template flows.Template, features []interf
 		pe.templates[id] = templateID
 	} else {
 		// resend template?
-		if time.Since(pe.templateLastSent) > time.Second*15 {
+		if time.Since(pe.templateLastSent) > pe.interval {
 			pe.templateLastSent = time.Now()
 			pe.writer.SendTemplate(when, id)
 			pe.writer.Flush(when)
@@ -70,35 +71,25 @@ func (pe *ipfixStreamExporter) ID() string {
 }
 
 func (pe *ipfixStreamExporter) AllocateIE(ies []ipfix.InformationElement) []ipfix.InformationElement {
-	result := make([]ipfix.InformationElement, len(ies))
 	for i, ie := range ies {
 		if ie.ID == 0 && ie.Pen == 0 { //Temporary Element
 			if ie, ok := pe.allocated[ie.Name]; ok {
-				result[i] = ie
+				ies[i] = ie
 				continue
 			}
 			name := ie.Name
-			newIe := ipfix.InformationElement{
+			ie = ipfix.InformationElement{
 				Name:   normalizeName(name),
 				Pen:    pen,
 				ID:     uint16(len(pe.allocated)) + tmpBase,
-				Type:   ipfix.Type(ie.Type),
+				Type:   ie.Type,
 				Length: ie.Length,
 			}
-			result[i] = newIe
-			pe.allocated[name] = newIe
-		} else {
-			newIe := ipfix.InformationElement{
-				Name:   normalizeName(ie.Name),
-				Pen:    ie.Pen,
-				ID:     ie.ID,
-				Type:   ipfix.Type(ie.Type),
-				Length: ie.Length,
-			}
-			result[i] = newIe
+			ies[i] = ie
+			pe.allocated[name] = ie
 		}
 	}
-	return result
+	return ies
 }
 
 func (pe *ipfixStreamExporter) Init() {
@@ -124,17 +115,25 @@ func newipfixStreamExporter(args []string) (arguments []string, ret util.Module,
 	set.Usage = func() { ipfixStreamHelp("ipfix_stream") }
 	protocol := set.String("protocol", "udp", "transport protocol")
 	domainStr := set.String("domain", "1", "observation domain")
+	intervalStr := set.String("template_interval", "300", "template resend interval")
 
 	set.Parse(args)
-	if set.NArg() < 2 {
+	if set.NArg() < 1 {
 		return nil, nil, errors.New("IPFIX exporter needs a target as argument")
 	}
 	target := set.Args()[0]
 
 	domain, err := strconv.Atoi(*domainStr)
+	if err != nil {
+		return
+	}
+	interval, err := strconv.Atoi(*intervalStr)
+	if err != nil {
+		return
+	}
 
 	ipfix.LoadIANASpec()
-	ret = &ipfixStreamExporter{id: "IPFIX|" + target, target: target, protocol: *protocol, domain: uint32(domain)}
+	ret = &ipfixStreamExporter{id: "IPFIX|" + target, target: target, protocol: *protocol, interval: time.Duration(interval) * time.Second, domain: uint32(domain)}
 	return
 }
 
@@ -146,13 +145,15 @@ header consisting of the feature description.
 As argument, the output target is needed.
 
 Usage:
-  export [-protocol udp/tcp] [-domain 42] %s target
+  export [-protocol udp/tcp] [-domain 42] [-template_interval 300] %s target
 
 Flags:
   -protocol string
     	Protocol to use to send data to a collector. Can be TCP or UDP
-  - domain int
+  -domain int
 		Observation domain to report by
+  -template_interval int
+		Delay in seconds before the template is send 
 
 
 `, name, name)
